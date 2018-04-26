b86d0181a49580546c88d033031985dbd3999380
Binoj Vijayakumar <binojvijayakumar@gmail.com>
2018-04-26 11:28:37 +0530

    identify taxportal

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,5 +1,17 @@
 chrome.runtime.onMessage.addListener(function (msg, sender) {
   if ((msg.from === 'content') && (msg.subject === 'showBadge')) {
     chrome.browserAction.setBadgeText({ "text": 'OK' });
+
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+      chrome.browserAction.setPopup({ tabId: tabs[0].id, popup: 'src/popup/popup.html' });
+    });
+
   }
+});
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  alert('Taxportal not deteected.');
 });
